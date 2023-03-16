5df1f7dc5e0b8e3409ac5faf101f1d6bba16b4e7
simaraconceicao <contato@simaraconceicao.com>
2023-03-16 16:26:26 -0300

    fix the type error3

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const express = require('express')
 const router = express.Router()
+const nomes = require('./database.json')
 
 const app = express()
 const PORT = 3333
 
 app.use(express.json())
 
-const nomes = [
-  { 
-    id: '1',
-    nome: 'Marielle Franco' 
-  },
-  { 
-    id: '2',
-    nome: 'Lélia Gonzalez' 
-  },
-  { 
-    id: '3',
-    nome: 'Angela Davis' 
-  }
-]
 
-const listaNomes = router.get('/', (res) => {
-  res.send({ lista: nomes })
+// const nomes = [
+//   { 
+//     id: '1',
+//     nome: 'Marielle Franco' 
+//   },
+//   { 
+//     id: '2',
+//     nome: 'Lélia Gonzalez' 
+//   },
+//   { 
+//     id: '3',
+//     nome: 'Angela Davis' 
+//   }
+// ]
+
+const listaNomes = router.get('/', (req, res) => {
+  res.json({ "data": nomes })
 })
 
 const criaNome = router.post('/', (req, res) => {
